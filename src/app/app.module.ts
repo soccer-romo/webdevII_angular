08d53e7116df4450d80a6832891b65b3159efa2e
NICULCEA <dn0885594@otc.edu>
2019-11-14 13:08:31 -0600

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchresultsComponent } from './searchresults/searchresults.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchresultsComponent
   ],
   imports: [
     BrowserModule,
